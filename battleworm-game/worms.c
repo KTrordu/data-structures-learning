5bb6efdf92ccf2753a1e29f863872e11ff34a349
KTrordu <alptug.yilmaz@outlook.com>
2025-07-14 00:47:34 +0300

    added killing a worm at battleworm-game

--- battleworm-game/worms.c
+++ battleworm-game/worms.c
@@ -128,6 +128,8 @@ struct DoublyList* create_worm(char* filename) {
     return worm;
 }
 
+int find_node_index(struct DoublyList* list, int x, int y);
+
 int main() {
     struct DoublyList* wormfield[100];
     int wormcount = 4;
@@ -170,6 +172,76 @@ int main() {
             int x, y;
             scanf("%d %d", &x, &y);
 
+            int hit_index = -1;
+            int hit_worm_index = -1;
+
+            for (int i = 0; i < wormcount; i++)
+            {
+                hit_index = find_node_index(wormfield[i], x, y);
+                if (hit_index != -1)
+                {
+                    hit_worm_index = i;
+                    break;
+                }
+            }
+
+            if(hit_worm_index == -1)
+            {
+                printf("Hit missed!\n");
+            }
+            else
+            {
+                printf("Hit! Worm %d at position %d\n", hit_worm_index, hit_index);
+
+                int length = wormfield[hit_worm_index]->elemcount;
+
+                int remove_start = hit_index - 1;
+                int remove_end = hit_index + 1;
+
+                if(remove_start < 0) remove_start = 0;
+                if(remove_end >= length) remove_end = length - 1;
+
+                int left_remaining = remove_start;
+                int right_remaining = length - (remove_end + 1);
+                printf("Debug: remove_start=%d, remove_end=%d\n", remove_start, remove_end);
+                printf("Debug: left_remaining=%d, right_remaining=%d\n", left_remaining, right_remaining);
+
+                if (left_remaining == 0 && right_remaining == 0)
+                {
+                    // worm öldü
+                    printf("Worm died completely!\n");
+
+                    while (wormfield[hit_worm_index]->head != NULL)
+                    {
+                        removeFront(wormfield[hit_worm_index]);
+                    }
+                    free(wormfield[hit_worm_index]);
+                    wormfield[hit_worm_index] = NULL;
+
+                    for (int i = hit_worm_index; i < wormcount - 1; i++)
+                    {
+                        wormfield[i] = wormfield[i + 1];
+                    }
+                    wormfield[wormcount - 1] = NULL;
+                    wormcount--;
+                }
+                else if(left_remaining <= 0 && right_remaining > 0)
+                {
+                    // worm'ün sol kısmı gitti
+                    printf("Left part destroyed, right part survives\n");
+                }
+                else if(left_remaining > 0 && right_remaining <= 0)
+                {
+                    // worm'ün sağ kısmı gitti
+                    printf("Right part destroyed, left part survives\n");
+                }
+                else
+                {
+                    // worm ikiye ayrıldı
+                    printf("Worm split into two parts!\n");
+                }
+            }
+
         } else if (option == 3) {
             break;
         } else {
@@ -178,4 +250,19 @@ int main() {
     }
 
     return 0;
+}
+
+int find_node_index(struct DoublyList* list, int x, int y)
+{
+    if(list == NULL || list->head == NULL) return -1;
+
+    struct Node *current = list->head;
+    for (int i = 0; i < list->elemcount; i++)
+    {
+        if (current->data->x == x && current->data->y == y) return i;
+
+        current = current->next;
+    }
+
+    return -1;
 }
